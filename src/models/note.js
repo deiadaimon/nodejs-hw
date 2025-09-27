9b665f4814f0eea966e5997401dc16523b96df83
Deia Daimon <deiadaimon@gmail.com>
2025-09-27 06:28:30 +0200

    02-mongodb

---
+++ src/models/note.js
@@ -0,0 +1,28 @@
+import { model } from "mongoose";
+import { Schema } from "mongoose";
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      default: "",
+    },
+    tag: {
+      type: String,
+      required: false,
+      enum: ["Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"],
+      default: "Todo",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model("Note", noteSchema);
