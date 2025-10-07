12b76cf032cabb252e411f05144f5ab48163604f
Deia Daimon <deiadaimon@gmail.com>
2025-10-07 22:13:41 +0200

    add validation

---
+++ src/validations/notesValidation.js
@@ -0,0 +1,41 @@
+import { Joi, Segments } from "celebrate";
+import { isValidObjectId } from "mongoose";
+
+export const getAllNotesSchema = {
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid("Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"),
+    search: Joi.string().allow(""),
+  })
+};
+
+const objectIdValidator = (value, helpers) => {
+  const isValidId = isValidObjectId(value);
+  return !isValidId ? helpers.message("Invalid ID value!") : value;
+};
+
+export const noteIdSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  })
+};
+
+export const createNoteSchema = {
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow(""),
+    tag: Joi.string().valid("Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo").required(),
+  })
+};
+
+export const updateNoteSchema = {
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(objectIdValidator).required(),
+  }),
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1),
+    content: Joi.string().allow(""),
+    tag: Joi.string().valid("Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"),
+  }).min(1)
+};
