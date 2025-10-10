2a7f3f59fc2564c5e25f8d9c43a44f4fb69c5ca3
Deia Daimon <deiadaimon@gmail.com>
2025-10-10 11:06:25 +0200

    fix TAGS

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,11 +1,12 @@
 import { Joi, Segments } from "celebrate";
 import { isValidObjectId } from "mongoose";
+import { TAGS } from "../constants/tags.js";
 
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid("Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"),
+    tag: Joi.string().valid(...TAGS),
     search: Joi.string().trim().allow(""),
   })
 };
@@ -25,7 +26,7 @@ export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(""),
-    tag: Joi.string().valid("Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo").required(),
+    tag: Joi.string().valid(...TAGS),
   })
 };
 
@@ -36,6 +37,6 @@ export const updateNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1),
     content: Joi.string().allow(""),
-    tag: Joi.string().valid("Work", "Personal", "Meeting", "Shopping", "Ideas", "Travel", "Finance", "Health", "Important", "Todo"),
+    tag: Joi.string().valid(...TAGS),
   }).min(1)
 };
