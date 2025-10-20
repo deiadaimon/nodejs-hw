3658e0b772cab50a5db71b72a91cb3d2d269fba1
Deia Daimon <deiadaimon@gmail.com>
2025-10-20 11:03:34 +0200

    fix issues

--- src/models/session.js
+++ src/models/session.js
@@ -3,6 +3,7 @@ import { model, Schema } from "mongoose";
 const sessionSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
+    ref: "User",
     required: true,
   },
   accessToken: {
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import authRoutes from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
 app.use(express.json({
