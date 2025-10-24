207832d48458e2165af99fe28b4277c79eda7f40
Deia Daimon <deiadaimon@gmail.com>
2025-10-24 05:57:12 +0200

    add avatar upload

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
 import { errors } from "celebrate";
 import { errorHandler } from "./middleware/errorHandler.js";
+import userRoutes from "./routes/userRoutes.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import cookieParser from "cookie-parser";
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 app.use(notFoundHandler);
 app.use(errors());
 app.use(errorHandler);
